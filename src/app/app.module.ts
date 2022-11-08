ecc1221c6d986eeea30f4473934f02494f18263b
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-08 09:28:50 +0100

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './MesComposants/toolbar/toolbar.component';
 import { ListComponent } from './MesComposants/list/list.component';
+import { FooterComponent } from './MesComposants/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     ListComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
