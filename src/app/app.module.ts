28ff64d550c1b45aab21673e27ae5be9f9f3ddca
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-08 09:14:07 +0100

    troisieme composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PremierComponent } from './premier/premier.component';
 import { DeuxiemeComponent } from './deuxieme/deuxieme.component';
+import { TroisiemeComponent } from './troisieme/troisieme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PremierComponent,
-    DeuxiemeComponent
+    DeuxiemeComponent,
+    TroisiemeComponent
   ],
   imports: [
     BrowserModule,
