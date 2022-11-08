8d01124061c2f919920b292ab58bbbe83784633d
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-08 09:27:06 +0100

    list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './MesComposants/toolbar/toolbar.component';
+import { ListComponent } from './MesComposants/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
