76906b8de7d5fc3ee5955428c17d0b37e2eed9fb
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-08 09:16:14 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PremierComponent } from './premier/premier.component';
-import { DeuxiemeComponent } from './deuxieme/deuxieme.component';
-import { TroisiemeComponent } from './troisieme/troisieme.component';
-import { QuatriemeComponent } from './MesComposants/quatrieme/quatrieme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PremierComponent,
-    DeuxiemeComponent,
-    TroisiemeComponent,
-    QuatriemeComponent
   ],
   imports: [
     BrowserModule,
