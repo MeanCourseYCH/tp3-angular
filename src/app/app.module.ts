2ecf92d64ef13674c351184962ab4977a400bcd1
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-08 09:03:02 +0100

    create new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PremierComponent } from './premier/premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierComponent
   ],
   imports: [
     BrowserModule,
